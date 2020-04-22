0ced0bf15edb24ef5f759aeaaf550fd25dbb5106
Alejandro <alejo.pm92@gmail.com>
2020-04-22 14:30:15 -0500

    [update] update actual sent code

--- python/alejo/leetcode_reverse_int.py
+++ python/alejo/leetcode_reverse_int.py
@@ -8,14 +8,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if len(bin(x)[2:]) <=32:
-            is_negative = x < 0
-            my_number_str = str(abs(x))
-            my_number_str = reduce(
-                lambda number, letter: number + letter, my_number_str[::-1]
-            )
 
-            reversed_number = int(my_number_str) * -1 if is_negative else int(my_number_str)
+        is_negative = x < 0
+        my_number_str = str(abs(x))
+        my_number_str = reduce(
+            lambda number, letter: number + letter, my_number_str[::-1]
+        )
 
+        reversed_number = int(my_number_str) * -1 if is_negative else int(
+            my_number_str)
+
+        my_range = 2 ** 31
+        negative_range = -1 * 2 ** 31
+        if negative_range < reversed_number < (my_range - 1):
             return reversed_number
+
         return 0
+
