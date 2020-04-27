ba6eecaab1e84f0d71af8baec934c56c5b0a5e8f
Alejandro <alejo.pm92@gmail.com>
2020-04-26 23:55:26 -0500

    [update] read the fknig problem it was 'a' not the first char

--- python/alejo/hackerrank_repeated_string.py
+++ python/alejo/hackerrank_repeated_string.py
@@ -1,25 +1,28 @@
 #!/bin/python3
 import os
-import math
 
 # Complete the repeatedString function below.
-from functools import reduce
+
+A = 'a'
 
 
 def repeated_string(my_str: str, final_len: int) -> int:
     """
-    Get a string and sum itself until reach the length that is passed in
-    repeated arg
+    Get a string and set the amount of a inside that string and then
+    multiplies by the value of the potential substring
     :param my_str: The string to evaluate
     :param final_len: The len of the string
     :return: The number of time that the initial string is repeated in the
     final string
     """
+
     if my_str:
-        many_times = math.ceil(final_len/len(my_str))
-        final_str = my_str*many_times
-        final_str = final_str[:final_len]
-        return final_str.count(my_str[0])
+        many_times = final_len//len(my_str)
+        my_char = my_str.count(A)
+
+        count_decimal = my_str[:final_len % len(my_str)].count(A)
+        repeated_times = my_char*many_times + count_decimal
+        return repeated_times
 
 
 if __name__ == '__main__':
