46ed98fe0794197c04bd6d902106cd69bcbae902
Alejandro <alejo.pm92@gmail.com>
2020-05-01 23:59:07 -0500

    [add] basics exercises for certifications hackerrank

---
+++ python/alejo/hackerrank_max_vowels.py
@@ -0,0 +1,50 @@
+from functools import reduce
+
+VOWELS = ['a', 'e', 'i', 'o', 'u']
+
+
+def find_substring(my_string: str, my_length: int):
+    how_many_vowels = reduce(
+        lambda acc, vowel: acc + my_string.count(vowel),
+        VOWELS, 0
+    )
+    if how_many_vowels:
+        vowels_str = {}
+        vowels_str.update(get_strings(my_string, my_length))
+        print(vowels_str)
+        vowels_str = {
+            k: v for k, v in sorted(
+                vowels_str.items(), reverse=True, key=lambda item: item[1]
+            )
+        }
+        return next(iter(vowels_str))
+    return 'Not found!'
+
+def get_strings(my_string: str, my_length: int) -> dict:
+    strings = {}
+    for index, _ in enumerate(my_string):
+        new_string = my_string[index:]
+        if len(new_string) >= my_length:
+            while len(new_string) > my_length:
+                new_string = new_string[1:]
+
+            number_of_vowels = reduce(
+                lambda acc, vowel: acc + new_string.count(vowel),
+                VOWELS, 0
+            )
+            strings[new_string] = number_of_vowels
+
+        new_string = my_string[: -index]
+
+        if len(new_string) >= my_length:
+            while len(new_string) > my_length:
+                new_string = new_string[:-1]
+
+            number_of_vowels = reduce(
+                lambda acc, vowel: acc + new_string.count(vowel),
+                VOWELS, 0
+            )
+            strings[new_string] = number_of_vowels
+
+    return strings
+
