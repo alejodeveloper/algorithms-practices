f60dc83ce7956c046251e1e510f30367918fe7f8
Alejandro <alejo.pm92@gmail.com>
2020-05-25 13:22:12 -0500

    [update] add deletion method for binaries trees

--- python/alejo/binary_threes_basics.py
+++ python/alejo/binary_threes_basics.py
@@ -44,3 +44,34 @@ class Node:
 
         if self.right:
             self.right.print_three()
+
+    def delete_node(self, data):
+        if self.data == data:
+            predecessor = self.get_predecessor(self)
+            self.data = predecessor
+
+        if self.data < data:
+            self.left.delete_node(data)
+
+        elif self.data > data:
+            self.right.delete(data)
+        else:
+            print("404 Not found")
+
+    def get_predecessor(self, prev_node):
+        if self.right:
+            predecessor = self.right.get_predecessor(self)
+
+            return predecessor
+
+        if self.left:
+            predecessor = self.data
+            self.data = self.left.data
+            self.left = self.left.left
+            self.right = self.left.rigth
+
+            return predecessor
+
+        prev_node.right = None
+        predecessor = self.data
+        return predecessor
