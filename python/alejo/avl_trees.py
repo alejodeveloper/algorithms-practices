6c27f4138cfdd9a93708289da76e8a8accb89b58
Alejo <sespitia@platzi.com>
2020-06-07 17:37:51 -0500

    [update] add deletion method

--- python/alejo/avl_trees.py
+++ python/alejo/avl_trees.py
@@ -13,7 +13,7 @@ class AVLTree:
             return self.print_avl(self.root)
 
         return ""
-    
+
     def print_avl(self, node: Node) -> str:
         str_tree = ""
         if node.left:
@@ -71,6 +71,35 @@ class AVLTree:
             return self.left_rotate(node)
         return node
 
+    def settle_violation_delete(self, node: Node) -> Node:
+        balance = self.calculate_balance(node)
+        if balance > 1 and self.calculate_balance(node.left) >= 0:
+            print("Rotating to the right.... three linked nodes on the left "
+                  "after delete")
+            print(f"Node {node.data}, left node child {node.left.data}, ")
+            return self.right_rotate(node)
+        elif balance < -1 and self.calculate_balance(node.right) >= 0:
+            print("Rotating to the left.... three linked nodes on the right "
+                  "after delete")
+            print(f"Node {node.data}, right node child {node.right.data}, ")
+            return self.left_rotate(node)
+
+        elif balance > 1 and self.calculate_balance(node.left) < 0:
+            print("Unbalance in the left on the right... rotating the root "
+                  "node, after delete")
+            print(f"Node data {node.data}, left {node.left.data}")
+            node.left = self.left_rotate(node.left)
+            return self.right_rotate(node)
+
+        elif balance < -1 and self.calculate_balance(node.right) < 0:
+            print("Unbalance in the right on the left... rotating the root "
+                  "node, after delete")
+            print(f"Node data {node.data}, right {node.right.data}")
+            node.right = self.right_rotate(node.right)
+            return self.left_rotate(node)
+
+        return node
+
     @staticmethod
     def node_height(node: Node):
         if not node:
@@ -112,3 +141,48 @@ class AVLTree:
         right_node.height = self.calculate_height(right_node)
 
         return right_node
+
+    def remove(self, data):
+        if self.root:
+            self.delete_node(data, self.root)
+
+    def delete_node(self, data, node: Node) -> Node:
+        if not node:
+            return node
+
+        if data > node.data:
+            node.right = self.delete_node(data, node.right)
+
+        elif data < node.data:
+            node.left = self.delete_node(data, node.left)
+
+        elif data == node.data:
+            if not node.left and not node.right:
+                print(f"Removing leaf {data} located into de node {node.data}")
+                return None
+
+            elif not node.left:
+                print(f"Removing node {data} with only a right child "
+                      f"{node.right.data}")
+                return node.right
+
+            elif not node.right:
+                print(f"Removing node {data} with only a left child "
+                      f"{node.left.data}")
+                return node.left
+
+            else:
+                predecessor = self.get_predecessor(node.left)
+                node.data = predecessor.data
+                node.left = self.delete_node(predecessor.data, node.left)
+
+        node.height = self.calculate_height(node)
+
+        return self.settle_violation_delete(node)
+
+    def get_predecessor(self, node: Node) -> Node:
+        if node.right:
+            return self.get_predecessor(node.right)
+
+        print(f"Returning predecessor {node.data}")
+        return node
